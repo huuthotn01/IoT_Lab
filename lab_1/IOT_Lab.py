8607f32ff3279382a59bf723300682306ce8cc52
huuthotn01 <tho.trannguyenhuu@hcmut.edu.vn>
2022-01-30 14:42:07 +0700

    First

---
+++ lab_1/IOT_Lab.py
@@ -0,0 +1,57 @@
+print("Hello ThingsBoard")
+import paho.mqtt.client as mqttclient
+import time
+import json
+
+BROKER_ADDRESS = "demo.thingsboard.io"
+PORT = 1883
+THINGS_BOARD_ACCESS_TOKEN = "FfMdTWqbg0x2fiZpR06r"
+
+
+def subscribed(client, userdata, mid, granted_qos):
+    print("Subscribed...")
+
+
+def recv_message(client, userdata, message):
+    print("Received: ", message.payload.decode("utf-8"))
+    temp_data = {'value': True}
+    try:
+        jsonobj = json.loads(message.payload)
+        if jsonobj['method'] == "setValue":
+            temp_data['value'] = jsonobj['params']
+            client.publish('v1/devices/me/attributes', json.dumps(temp_data), 1)
+    except:
+        pass
+
+
+def connected(client, usedata, flags, rc):
+    if rc == 0:
+        print("Thingsboard connected successfully!!")
+        client.subscribe("v1/devices/me/rpc/request/+")
+    else:
+        print("Connection is failed")
+
+
+client = mqttclient.Client("Gateway_Thingsboard")
+client.username_pw_set(THINGS_BOARD_ACCESS_TOKEN)
+
+client.on_connect = connected
+client.connect(BROKER_ADDRESS, 1883)
+client.loop_start()
+
+client.on_subscribe = subscribed
+client.on_message = recv_message
+
+longitude = 106.6297
+latitude = 10.8231
+temp = 30
+humid = 50
+light_intensity = 100
+counter = 0
+while True:
+    collect_data = {'temperature': temp, 'humidity': humid, 'light':light_intensity, 'longitude': longitude, 'latitude': latitude}
+    temp += 1
+    humid += 1
+    light_intensity += 1
+    client.publish('v1/devices/me/telemetry', json.dumps(collect_data), 1)
+    time.sleep(10)
