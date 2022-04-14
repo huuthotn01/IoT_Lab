55a116797601ee662eb490558a179c7617294492
huuthotn01 <tho.trannguyenhuu@hcmut.edu.vn>
2022-04-14 20:46:12 +0700

    Dashboard

---
+++ lab_2/Assets/M2MqttUnity/Examples/Scripts/M2MqttUnityTest.cs
@@ -0,0 +1,279 @@
+﻿/*
+The MIT License (MIT)
+
+Copyright (c) 2018 Giovanni Paolo Vigano'
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using M2MqttUnity;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Examples for the M2MQTT library (https://github.com/eclipse/paho.mqtt.m2mqtt),
+/// </summary>
+namespace M2MqttUnity.Examples
+{
+    public class SensorData {
+        public string temp {get; set;}
+        public string humid {get; set;}
+    }
+    /// <summary>
+    /// Script for testing M2MQTT with a Unity UI
+    /// </summary>
+    public class M2MqttUnityTest : M2MqttUnityClient
+    {
+        [Tooltip("Set this to true to perform a testing cycle automatically on startup")]
+        public bool autoTest = false;
+        [Header("User Interface")]
+        public GameObject SignIn;
+        public GameObject TestUI;
+        public InputField consoleInputField;
+        public InputField addressInputField;
+        public InputField addressInputFieldUsername;
+        public InputField addressInputFieldPass;
+        public InputField portInputField;
+        public Button connectButton;
+        public InputField InputFieldTemp;
+        public InputField InputFieldHumid;
+
+        private List<string> eventMessages = new List<string>();
+        private bool updateUI = false;
+
+        public void TestPublish()
+        {
+            client.Publish("/bkiot/1915347/status", System.Text.Encoding.UTF8.GetBytes("Test message"), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+            Debug.Log("Test message published");
+            AddUiMessage("Test message published.");
+        }
+
+        public void SetBrokerAddress(string brokerAddress)
+        {
+            if (addressInputField && !updateUI)
+            {
+                this.brokerAddress = brokerAddress;
+            }
+        }
+
+        public void SetUsername(string username)
+        {
+            if (addressInputFieldUsername && !updateUI)
+            {
+                this.mqttUserName = username;
+            }
+        }
+
+        public void SetPass(string pass)
+        {
+            if (addressInputFieldPass && !updateUI)
+            {
+                this.mqttPassword = pass;
+            }
+        }
+
+        public void SetBrokerPort(string brokerPort)
+        {
+            if (portInputField && !updateUI)
+            {
+                int.TryParse(brokerPort, out this.brokerPort);
+            }
+        }
+
+
+        public void SetUiMessage(string msg)
+        {
+            if (consoleInputField != null)
+            {
+                consoleInputField.text = msg;
+                updateUI = true;
+            }
+        }
+
+        public void AddUiMessage(string msg)
+        {
+            if (consoleInputField != null)
+            {
+                consoleInputField.text += msg + "\n";
+                updateUI = true;
+            }
+        }
+
+        protected override void OnConnecting()
+        {
+            base.OnConnecting();
+            SetUiMessage("Connecting to broker on " + brokerAddress + ":" + brokerPort.ToString() + "...\n");
+        }
+
+        protected override void OnConnected()
+        {
+            base.OnConnected();
+            
+            SetUiMessage("Connected to broker on " + brokerAddress + "\n");
+            SubscribeTopics();
+            if (InputFieldHumid != null && InputFieldTemp != null) {
+                InputFieldTemp.text = "55°C";
+                InputFieldHumid.text = "17%";
+            }
+            SignIn.SetActive(false);
+            TestUI.SetActive(true);
+            /*if (autoTest)
+            {
+                TestPublish();
+            }*/
+        }
+
+        protected override void SubscribeTopics()
+        {
+            client.Subscribe(new string[] { "/bkiot/1915347/status" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+            Debug.Log("Test");
+        }
+
+        protected override void UnsubscribeTopics()
+        {
+            client.Unsubscribe(new string[] { "/bkiot/1915347/status" });
+        }
+
+        protected override void OnConnectionFailed(string errorMessage)
+        {
+            AddUiMessage("CONNECTION FAILED! " + errorMessage);
+        }
+
+        protected override void OnDisconnected()
+        {
+            AddUiMessage("Disconnected.");
+        }
+
+        protected override void OnConnectionLost()
+        {
+            AddUiMessage("CONNECTION LOST!");
+        }
+
+        private void UpdateUI()
+        {
+            if (client == null)
+            {
+                if (connectButton != null)
+                {
+                    connectButton.interactable = true;
+                }
+            }
+            else
+            {
+                if (connectButton != null)
+                {
+                    connectButton.interactable = !client.IsConnected;
+                }
+            }
+            if (addressInputField != null && connectButton != null)
+            {
+                addressInputField.interactable = connectButton.interactable;
+                addressInputField.text = brokerAddress;
+            }
+            if (addressInputFieldUsername != null && connectButton != null)
+            {
+                addressInputFieldUsername.interactable = connectButton.interactable;
+                addressInputFieldUsername.text = mqttUserName;
+            }
+            if (addressInputFieldPass != null && connectButton != null)
+            {
+                addressInputFieldPass.interactable = connectButton.interactable;
+                addressInputFieldPass.text = mqttPassword;
+            }
+            if (portInputField != null && connectButton != null)
+            {
+                portInputField.interactable = connectButton.interactable;
+                portInputField.text = brokerPort.ToString();
+            }
+            updateUI = false;
+        }
+
+        protected override void Start()
+        {
+            SignIn.SetActive(true);
+            TestUI.SetActive(false);
+            SetUiMessage("Ready.");
+            updateUI = true;
+            base.Start();
+        }
+
+        protected override void DecodeMessage(string topic, byte[] message)
+        {
+            Debug.Log("Hello Test");
+            string msg = System.Text.Encoding.UTF8.GetString(message);
+            Debug.Log("Received: " + msg);
+            StoreMessage(msg);
+            if (topic == "/bkiot/1915347/status")
+            {
+                if (autoTest)
+                {
+                    autoTest = false;
+                    Disconnect();
+                }
+            }
+        }
+
+        private void StoreMessage(string eventMsg)
+        {
+            eventMessages.Add(eventMsg);
+        }
+
+        private void ProcessMessage(string msg)
+        {
+            AddUiMessage("Received: " + msg);
+        }
+
+        protected override void Update()
+        {
+            base.Update(); // call ProcessMqttEvents()
+
+            if (eventMessages.Count > 0)
+            {
+                foreach (string msg in eventMessages)
+                {
+                    ProcessMessage(msg);
+                }
+                eventMessages.Clear();
+            }
+            if (updateUI)
+            {
+                UpdateUI();
+            }
+        }
+
+        private void OnDestroy()
+        {
+            //Disconnect();
+        }
+
+        private void OnValidate()
+        {
+            if (autoTest)
+            {
+                autoConnect = true;
+            }
+        }
+    }
+}
